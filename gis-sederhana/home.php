48320ea01d6a573f202405d687af9dbcb09b0da3
iqbal51 <iqbalarrahman51@gmail.com>
2019-04-22 13:56:13 +0700

    update

---
+++ gis-sederhana/home.php
@@ -0,0 +1,6 @@
+<div class="page-header">
+    <h1></h1>
+</div>
+<body>
+<img src="" align="right" />
+</body>
---
+++ gis-sederhana/login.php
@@ -0,0 +1,22 @@
+<div class="page-header">
+    <h1>Login</h1>
+</div>
+<body>
+<img src="bpn.png" height="100px" width="100px" align="left" />
+</body>
+<div class="row">
+    <div class="col-md-4">        
+        <?php if($_POST) include 'aksi.php';?>
+        <form class="form-signin" action="?m=login" method="post">              
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" placeholder="Username" name="user" autofocus />
+            </div>
+            <div class="form-group">            
+                <label>Password</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="pass" />  
+            </div>      
+            <button class="btn btn-primary btn-block" type="submit"><span class="glyphicon glyphicon-log-in"></span> Masuk</button>        
+        </form>      
+    </div>
+</div>
